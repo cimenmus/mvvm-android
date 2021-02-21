6875af03cee607d2b812d5f196eaf57acb3cc191
mustafaicmen <mustafaicmen@gmail.com>
2021-02-21 12:57:45 +0000

    Version bump : versionCode = 23 | versionName = 1.0.9

---
+++ vendor/bundle/ruby/2.6.0/gems/fastlane-plugin-firebase_app_distribution-0.2.5/lib/fastlane/plugin/firebase_app_distribution/actions/firebase_app_distribution_action.rb
@@ -0,0 +1,218 @@
+require 'fastlane/action'
+require 'open3'
+require 'shellwords'
+require 'googleauth'
+require_relative '../helper/upload_status_response'
+require_relative '../helper/firebase_app_distribution_helper'
+require_relative '../helper/firebase_app_distribution_error_message'
+require_relative '../client/firebase_app_distribution_api_client'
+require_relative '../helper/firebase_app_distribution_auth_client'
+
+## TODO: should always use a file underneath? I think so.
+## How should we document the usage of release notes?
+module Fastlane
+  module Actions
+    class FirebaseAppDistributionAction < Action
+      FIREBASECMD_ACTION = "appdistribution:distribute".freeze
+
+      extend Auth::FirebaseAppDistributionAuthClient
+      extend Helper::FirebaseAppDistributionHelper
+
+      def self.run(params)
+        params.values # to validate all inputs before looking for the ipa/apk
+
+        app_id = app_id_from_params(params)
+        platform = lane_platform || platform_from_app_id(app_id)
+        binary_path = binary_path_from_platform(platform, params[:ipa_path], params[:apk_path])
+
+        auth_token = fetch_auth_token(params[:service_credentials_file], params[:firebase_cli_token])
+        fad_api_client = Client::FirebaseAppDistributionApiClient.new(auth_token, platform, params[:debug])
+
+        release_id = fad_api_client.upload(app_id, binary_path, platform.to_s)
+        if release_id.nil?
+          return
+        end
+
+        release_notes = get_value_from_value_or_file(params[:release_notes], params[:release_notes_file])
+        fad_api_client.post_notes(app_id, release_id, release_notes)
+
+        testers = get_value_from_value_or_file(params[:testers], params[:testers_file])
+        groups = get_value_from_value_or_file(params[:groups], params[:groups_file])
+        emails = string_to_array(testers)
+        group_ids = string_to_array(groups)
+        fad_api_client.enable_access(app_id, release_id, emails, group_ids)
+        UI.success("🎉 App Distribution upload finished successfully.")
+      end
+
+      def self.description
+        "Release your beta builds with Firebase App Distribution"
+      end
+
+      def self.authors
+        ["Stefan Natchev", "Manny Jimenez Github: mannyjimenez0810, Alonso Salas Infante Github: alonsosalasinfante"]
+      end
+
+      # supports markdown.
+      def self.details
+        "Release your beta builds with Firebase App Distribution"
+      end
+
+      def self.app_id_from_params(params)
+        if params[:app]
+          app_id = params[:app]
+        elsif xcode_archive_path
+          plist_path = params[:googleservice_info_plist_path]
+          app_id = get_ios_app_id_from_archive_plist(xcode_archive_path, plist_path)
+        end
+        if app_id.nil?
+          UI.crash!(ErrorMessage::MISSING_APP_ID)
+        end
+        app_id
+      end
+
+      def self.xcode_archive_path
+        # prevents issues on cross-platform build environments where an XCode build happens within
+        # the same lane
+        return nil if lane_platform == :android
+
+        Actions.lane_context[SharedValues::XCODEBUILD_ARCHIVE]
+      end
+
+      def self.lane_platform
+        Actions.lane_context[Actions::SharedValues::PLATFORM_NAME]
+      end
+
+      def self.platform_from_app_id(app_id)
+        if app_id.include?(':ios:')
+          :ios
+        elsif app_id.include?(':android:')
+          :android
+        end
+      end
+
+      def self.binary_path_from_platform(platform, ipa_path, apk_path)
+        case platform
+        when :ios
+          ipa_path
+        when :android
+          apk_path
+        else
+          ipa_path || apk_path
+        end
+      end
+
+      def self.available_options
+        if lane_platform == :ios || lane_platform.nil?
+          ipa_path_default = Dir["*.ipa"].sort_by { |x| File.mtime(x) }.last
+        end
+
+        if lane_platform == :android
+          apk_path_default = Dir["*.apk"].last || Dir[File.join("app", "build", "outputs", "apk", "app-release.apk")].last
+        end
+
+        [
+          # iOS Specific
+          FastlaneCore::ConfigItem.new(key: :ipa_path,
+                                       env_name: "FIREBASEAPPDISTRO_IPA_PATH",
+                                       description: "Path to your IPA file. Optional if you use the _gym_ or _xcodebuild_ action",
+                                       default_value: Actions.lane_context[SharedValues::IPA_OUTPUT_PATH] || ipa_path_default,
+                                       default_value_dynamic: true,
+                                       optional: true,
+                                       verify_block: proc do |value|
+                                         UI.user_error!("firebase_app_distribution: Couldn't find ipa file at path '#{value}'") unless File.exist?(value)
+                                       end),
+          FastlaneCore::ConfigItem.new(key: :googleservice_info_plist_path,
+                                       env_name: "GOOGLESERVICE_INFO_PLIST_PATH",
+                                       description: "Path to your GoogleService-Info.plist file, relative to the archived product path",
+                                       default_value: "GoogleService-Info.plist",
+                                       optional: true,
+                                       type: String),
+          # Android Specific
+          FastlaneCore::ConfigItem.new(key: :apk_path,
+                                       env_name: "FIREBASEAPPDISTRO_APK_PATH",
+                                       description: "Path to your APK file",
+                                       default_value: Actions.lane_context[SharedValues::GRADLE_APK_OUTPUT_PATH] || apk_path_default,
+                                       default_value_dynamic: true,
+                                       optional: true,
+                                       verify_block: proc do |value|
+                                         UI.user_error!("firebase_app_distribution: Couldn't find apk file at path '#{value}'") unless File.exist?(value)
+                                       end),
+          FastlaneCore::ConfigItem.new(key: :app,
+                                       env_name: "FIREBASEAPPDISTRO_APP",
+                                       description: "Your app's Firebase App ID. You can find the App ID in the Firebase console, on the General Settings page",
+                                       optional: true,
+                                       type: String),
+          FastlaneCore::ConfigItem.new(key: :firebase_cli_path,
+                                       deprecated: "This plugin no longer uses the Firebase CLI",
+                                       env_name: "FIREBASEAPPDISTRO_FIREBASE_CLI_PATH",
+                                       description: "The absolute path of the firebase cli command",
+                                       type: String),
+          FastlaneCore::ConfigItem.new(key: :groups,
+                                       env_name: "FIREBASEAPPDISTRO_GROUPS",
+                                       description: "The groups used for distribution, separated by commas",
+                                       optional: true,
+                                       type: String),
+          FastlaneCore::ConfigItem.new(key: :groups_file,
+                                       env_name: "FIREBASEAPPDISTRO_GROUPS_FILE",
+                                       description: "The groups used for distribution, separated by commas",
+                                       optional: true,
+                                       type: String),
+          FastlaneCore::ConfigItem.new(key: :testers,
+                                       env_name: "FIREBASEAPPDISTRO_TESTERS",
+                                       description: "Pass email addresses of testers, separated by commas",
+                                       optional: true,
+                                       type: String),
+          FastlaneCore::ConfigItem.new(key: :testers_file,
+                                       env_name: "FIREBASEAPPDISTRO_TESTERS_FILE",
+                                       description: "Pass email addresses of testers, separated by commas",
+                                       optional: true,
+                                       type: String),
+          FastlaneCore::ConfigItem.new(key: :release_notes,
+                                       env_name: "FIREBASEAPPDISTRO_RELEASE_NOTES",
+                                       description: "Release notes for this build",
+                                       default_value: Actions.lane_context[SharedValues::FL_CHANGELOG],
+                                       default_value_dynamic: true,
+                                       optional: true,
+                                       type: String),
+          FastlaneCore::ConfigItem.new(key: :release_notes_file,
+                                       env_name: "FIREBASEAPPDISTRO_RELEASE_NOTES_FILE",
+                                       description: "Release notes file for this build",
+                                       optional: true,
+                                       type: String),
+          FastlaneCore::ConfigItem.new(key: :firebase_cli_token,
+                                       description: "Auth token for firebase cli",
+                                       optional: true,
+                                       type: String),
+          FastlaneCore::ConfigItem.new(key: :debug,
+                                       description: "Print verbose debug output",
+                                       optional: true,
+                                       default_value: false,
+                                       is_string: false),
+          FastlaneCore::ConfigItem.new(key: :service_credentials_file,
+                                       description: "Path to Google service account json",
+                                       optional: true,
+                                       type: String)
+        ]
+      end
+
+      def self.is_supported?(platform)
+        # Adjust this if your plugin only works for a particular platform (iOS vs. Android, for example)
+        # See: https://docs.fastlane.tools/advanced/#control-configuration-by-lane-and-by-platform
+        #
+        # [:ios, :mac, :android].include?(platform)
+        true
+      end
+
+      def self.example_code
+        [
+          <<-CODE
+            firebase_app_distribution(
+              app: "1:1234567890:ios:0a1b2c3d4e5f67890",
+              testers: "snatchev@google.com, rebeccahe@google.com"
+            )
+          CODE
+        ]
+      end
+    end
+  end
+end
---
+++ vendor/bundle/ruby/2.6.0/gems/fastlane-plugin-firebase_app_distribution-0.2.5/lib/fastlane/plugin/firebase_app_distribution/version.rb
@@ -0,0 +1,5 @@
+module Fastlane
+  module FirebaseAppDistribution
+    VERSION = "0.2.5"
+  end
+end
